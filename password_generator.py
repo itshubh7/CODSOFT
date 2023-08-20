b25401d7a9209efb0b4699012e6ef38e12d3d784
itshubh7 <118055163+itshubh7@users.noreply.github.com>
2023-08-20 11:28:06 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -5,11 +5,11 @@ complexity = int(input('Choose the complexity of the password (1/2/3): ')) # Tak
 length = int(input("Enter the length of the password: ")) # Taking the length of the password
 
 if complexity == 1:
-    pswrd = string.ascii_letters
+    pswrd = string.ascii_letters # Only use letters as a password
 if complexity == 2:
-    pswrd = string.ascii_letters + string.digits
+    pswrd = string.ascii_letters + string.digits # Use letters and digits to build the password
 if complexity == 3:
-    pswrd = string.ascii_letters + string.punctuation + string.digits
+    pswrd = string.ascii_letters + string.punctuation + string.digits # Combines letters, digits and special characters
     
 password = ''
 
