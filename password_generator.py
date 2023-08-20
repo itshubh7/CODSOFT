1d0aeca6c522582a438c2d85bb68296cafa1ec7e
itshubh7 <118055163+itshubh7@users.noreply.github.com>
2023-08-20 11:57:47 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,21 +1,25 @@
 import random
 import string
 
-complexity = int(input('Choose the complexity of the password (1/2/3): ')) # Taking the complexity as an input
+def pw(length, pswrd): # Will create the password
+    password = ''
+
+    for i in range(length):
+        password += ''.join(random.choice(pswrd)) 
+    
+    print(f'Password is : {password}')
+
 length = int(input("Enter the length of the password: ")) # Taking the length of the password
+complexity = int(input('Choose the complexity of the password (1/2/3): ')) # Taking the complexity as an input
 
 if complexity == 1:
     pswrd = string.ascii_letters # Only use letters as a password
+    pw(length, pswrd)
 elif complexity == 2:
     pswrd = string.ascii_letters + string.digits # Use letters and digits to build the password
+    pw(length, pswrd)
 elif complexity == 3:
     pswrd = string.ascii_letters + string.punctuation + string.digits # Combines letters, digits and special characters
+    pw(length, pswrd)
 else:
-    print('Wrong Choice!')
-    
-password = ''
-
-for i in range(length):
-    password += ''.join(random.choice(pswrd)) 
-    
-print(f'Password is : {password}')
+    print('Wrong Choice!') # For wrong choices only
